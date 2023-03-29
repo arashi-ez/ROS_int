c8737c8618ec774eb1b0788fe918d3d881e8380d
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-03-29 15:26:36 +0500

    Initial commit

---
+++ bot_controller/scripts/my_first_time.py
@@ -0,0 +1,11 @@
+#/usr/bin/env python3
+import rospy
+
+if __name__ == '__main__':
+    rospy.init_node("test_nide")
+    rospy.logwarn("This is a warning")
+    rospy.logerr("Error")
+
+    rospy.sleep(1.0)
+
+    rospy.loginfo("GG")
