76ca64fb8684665ffc153844a5ee31f5330f7e19
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-03-29 17:56:41 +0500

    Update my_first_time.py

--- bot_controller/scripts/my_first_time.py
+++ bot_controller/scripts/my_first_time.py
@@ -2,5 +2,11 @@
 import rospy
 
 if __name__ == '__main__':
-    rospy.init_node("test_node")
+    rospy.init_node("galacts_first_time_test")
     rospy.loginfo("Test node has been started")
+
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        rospy.loginfo("Ez Pz")
+        rate.sleep()
