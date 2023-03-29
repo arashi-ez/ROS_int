c458862d1670dc2b1b6d473d54ea773936a3c0c8
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-03-29 17:45:32 +0500

    starting for scratch

--- bot_controller/scripts/my_first_time.py
+++ bot_controller/scripts/my_first_time.py
@@ -3,10 +3,4 @@ import rospy
 
 if __name__ == '__main__':
     rospy.init_node("test_node")
-    rospy.loginfo("GGs means goodbuy nabs")
-    rospy.logwarn("This is a warning")
-    rospy.logerr("Error")
-
-    rospy.sleep(1.0)
-
-    rospy.loginfo("GGs")
+    rospy.loginfo("Test node has been started")
