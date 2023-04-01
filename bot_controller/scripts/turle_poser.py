90d7a5c249bcb2b918464443b707cb07b10a2d1d
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-04-01 16:53:03 +0500

    [NEED TO BE DONE]

--- bot_controller/scripts/turle_poser.py
+++ bot_controller/scripts/turle_poser.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python3
-import rospy
-from turtlesim.msg import Pose 
+import rospy 
 from geometry_msgs.msg import Twist 
+from math import pi
 
-def pose_callback(pose : Pose):
-    cmd = Twist()
-    if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
-        cmd.linear.x = 1.0
-        cmd.angular.z = 1.2
-    else:
-        cmd.linear.x = 5.0
-        cmd.angular.z = 0.0
-    pub.publish(cmd)
+rospy.init_node('draw_star')
 
-if __name__=='__main__':
-    rospy.init_node("turtle_poser")
-    pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
-    sub = rospy.Subscriber("/turtle1/pose", Pose, callback=pose_callback) 
-    rospy.loginfo("NODE IS STARTING")
+turtle_vel = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+rate = rospy.Rate(1)
 
-    rospy.spin()
+while not rospy.is_shutdown():
+    # move forward
+    move_cmd = Twist()
+    move_cmd.linear.x = 1
+    turtle_vel.publish(move_cmd)
+    rospy.sleep(2)
+
+    # rotate for 144 degrees (2.5 * pi)
+    turn_cmd = Twist()
+    turn_cmd.angular.z = 2.5 * pi / 180
+    turtle_vel.publish(turn_cmd)
+    rospy.sleep(1.25)
+
+    # move forward again
+    turtle_vel.publish(move_cmd)
+    rospy.sleep(2)
+
+    # rotate for another 144 degrees
+    turtle_vel.publish(turn_cmd)
+    rospy.sleep(1.25)
+
+rospy.spin()
