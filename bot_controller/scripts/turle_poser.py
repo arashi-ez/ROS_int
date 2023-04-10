ced6eff98290bd6155744fb7e54d153291ca325e
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-04-10 10:56:41 +0500

    changes for per color

--- bot_controller/scripts/turle_poser.py
+++ bot_controller/scripts/turle_poser.py
@@ -2,6 +2,15 @@
 import rospy
 from turtlesim.msg import Pose 
 from geometry_msgs.msg import Twist 
+from turtlesim.srv import SetPen
+
+def call_set_pen_sevice(r, g, b, width, off):
+    try:
+        set_pen = rospy.ServiceProxy("/turtle1/set_pen", SetPen)
+        response = set_pen(r, g, b, width, off)
+        # rospy.loginfo(response)
+    except rospy.ServiceException as e:
+        rospy.logwarn(e)
 
 def pose_callback(pose : Pose):
     cmd = Twist()
@@ -15,6 +24,8 @@ def pose_callback(pose : Pose):
 
 if __name__=='__main__':
     rospy.init_node("turtle_poser")
+    rospy.wait_for_service("/turtle1/set_pen")
+    call_set_pen_sevice(255, 0, 0, 3, 0)
     pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
     sub = rospy.Subscriber("/turtle1/pose", Pose, callback=pose_callback) 
     rospy.loginfo("NODE IS STARTING")
