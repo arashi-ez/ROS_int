202dc1f21c6516e35045bce3dd0270f4b72a95f2
arashi-ez <122167794+arashi-ez@users.noreply.github.com>
2023-04-10 14:56:02 +0500

    NOT competed changes on turning the turtle

--- bot_controller/scripts/turle_poser.py
+++ bot_controller/scripts/turle_poser.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 import rospy
+import math
 from turtlesim.msg import Pose 
 from geometry_msgs.msg import Twist 
 
 def pose_callback(pose : Pose):
     cmd = Twist()
-    if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
-        cmd.linear.x = 1.0
-        cmd.angular.z = 1.2
+    #if pose.x > 9.0 or pose.x < 2.0 or pose.y > 9.0 or pose.y < 2.0:
+    #    cmd.linear.x = 0.0
+    #    cmd.angular.z = math.pi/math.radians(180) 
+
+    #U need to make a def which will move turtle after turning 180 degrees
+    if pose.x == 5.5: 
+        cmd.linear.x = 0.0
+        cmd.angular.z = math.pi/math.radians(180)
+    elif a: 
+        pass
     else:
-        cmd.linear.x = 5.0
+        cmd.linear.x = 2.0
         cmd.angular.z = 0.0
     pub.publish(cmd)
+    
 
 if __name__=='__main__':
     rospy.init_node("turtle_poser")
